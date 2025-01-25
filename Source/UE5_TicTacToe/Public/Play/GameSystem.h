78cd183fbbe78a72cb71ea966a51b2bfa26aab4d
Yan Liu <ly163com01@163.com>
2025-01-25 13:26:59 -0700

    game system add variables

--- Source/UE5_TicTacToe/Public/Play/GameSystem.h
+++ Source/UE5_TicTacToe/Public/Play/GameSystem.h
@@ -4,6 +4,9 @@
 #include "GameFramework/Actor.h"
 #include "GameSystem.generated.h"
 
+class ACell;
+class AAudioSystem;
+
 UENUM(BlueprintType)
 enum class EMark : uint8 {
 	None     UMETA(DisplayName = "None"),
@@ -12,6 +15,8 @@ enum class EMark : uint8 {
 };
 
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnStateChanged);
+
 UCLASS()
 class UE5_TICTACTOE_API AGameSystem : public AActor
 {
@@ -19,11 +24,51 @@ class UE5_TICTACTOE_API AGameSystem : public AActor
 	
 public:	
 	AGameSystem();
+	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	TObjectPtr<AAudioSystem> AudioSystem;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	bool Turn = true;  //Turn = true: player, Turn = false: AI
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	TArray<TObjectPtr<ACell>> Cells;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	TObjectPtr<ACell> ClickedCell;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	TArray<TObjectPtr<ACell>> WinCells;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	bool IsGameEnd = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	int M = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	int N = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	int K = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	EMark Winner = EMark::None;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	int AvailableMoves = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	int WinCountX = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	int WinCountO = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="CPP Settings")
+	float CellsOffsetPerc = 10.0f;
+	
+
+	//Delegate
+	UPROPERTY(BlueprintAssignable, Category = "CPP Settings")
+	FOnStateChanged OnGameStart;
+	UPROPERTY(BlueprintAssignable, Category = "CPP Settings")
+	FOnStateChanged OnGamePause;
+	UPROPERTY(BlueprintAssignable, Category = "CPP Settings")
+	FOnStateChanged OnGameEnd;
+	UPROPERTY(BlueprintAssignable, Category = "CPP Settings")
+	FOnStateChanged OnTurnChange;
+	
 protected:
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void Tick(float DeltaTime) override;
+	
 
 };
